a65ec95a39b9984b0752fa591b68fb8309285664
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2026-02-13 23:08:17 -0300

    cambios confg cloudinary

--- emuna-backend/src/config/cloudinaryConfig.js
+++ emuna-backend/src/config/cloudinaryConfig.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-// Configuración con tus credenciales (estarán en el .env)
+// Verifica que estos nombres coincidan con tus variables en Render
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -12,8 +12,9 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "emuna_productos", // Nombre de la carpeta en Cloudinary
+    folder: "emuna_productos",
     allowed_formats: ["jpg", "png", "jpeg"],
+    // Borra cualquier otra línea que tengas aquí por ahora
   },
 });
 
